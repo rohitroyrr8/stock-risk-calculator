b5e756bc86b3979b9bef0a22b789d50099f44d0a
Rohit Roy <rohitroyrr8@gmail.com>
2026-03-02 12:42:38 +0000

    minor syntax changes

---
+++ main.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import os
+from logic.risk_csv import env_reset
+import logic.risk_calculation as risk_calc
+from logic import csvutils as csv
+
+
+def get_param(name):
+    while True:
+        try:
+            value = float(input("{} : ".format(name)))
+            if value > 0:
+                return value
+            else:
+                print("{} should be more than zero.".format(name))
+        except ValueError:
+            print("Incorrect input. Please, try again.")
+
+
+def get_index_code(historical_prices):
+    while True:
+        index_symbol = input("Index symbol : ").upper()
+        if index_symbol in historical_prices.keys():
+            return index_symbol
+        else:
+            print("Index '{}' not found.".format(index_symbol))
+
+
+def main():
+    working_directory = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "historical-prices"
+    )
+
+    env_reset()
+
+    date_time_index = 0
+    prices_index = 4
+
+    balance = get_param("Balance $")
+    risk = get_param("Risk %")
+    commission = get_param("Commission %")
+
+    prices = csv.read_all_files(working_directory, date_time_index, prices_index)
+    index_symbol = get_index_code(prices)
+
+    rc = risk_calc.RiskCalculation(prices, index_symbol, r_value_min_abs=0.6)
+
+    symbols = rc.risk_params.keys()
+    risk_params = rc.risk_params.values()
+
+    data = {
+        'b1': [i.b1 for i in risk_params],
+        'r_value': [i.r_value for i in risk_params],
+        'weight': [i.weight for i in risk_params],
+        'trade_volume': [balance * i.weight for i in risk_params],
+        'risk_limit': [balance * i.weight * risk / 100.0 for i in risk_params],
+        'commission': [balance * i.weight * commission / 100.0 for i in risk_params]
+    }
+
+    df = pd.DataFrame(data, index=symbols)
+
+    cols = ['b1', 'r_value', 'weight', 'trade_volume', 'risk_limit', 'commission']
+    df = df[cols]
+
+    print(df)
+    input("\npush 'ENTER' to exit.")
+
+
+if __name__ == '__main__':
+    main()
